c9f660ca92e345ad8691e4f5014ddd80732425ac
khalilonline99 <khalilonline99@gmail.com>
2022-08-16 23:57:57 +0600

    withdraw and balance is working

--- js/balance.js
+++ js/balance.js
@@ -10,4 +10,31 @@ const totalDepositNumber = parseFloat(totalDepositString);
 const totalDepositAmount = depositAmountNumber + totalDepositNumber;
 totalDeposit.innerText = totalDepositAmount;
 depositField.value = '';
+
+const totalBalance = document.getElementById('balance-total-amount');
+const totalBalanceString = totalBalance.innerText;
+const totalBalanceNumber = parseFloat(totalBalanceString);
+const balanceTotal = depositAmountNumber + totalBalanceNumber;
+totalBalance.innerText = balanceTotal;
 })
+
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+  const withdrawField = document.getElementById('withdraw-field');
+  const withdrawFieldString = withdrawField.value;
+  const withdrawFieldNumber = parseFloat(withdrawFieldString);
+  withdrawField.value = '';
+
+  const totalWithdraw = document.getElementById('withdraw-total-field');
+  const totalWithdrawString = totalWithdraw.innerText;
+  const totalWithdrawNumber = parseFloat(totalWithdrawString);
+  const totalWithdrawAmount = withdrawFieldNumber + totalWithdrawNumber;
+  totalWithdraw.innerText = totalWithdrawAmount;
+
+  const totalBalance = document.getElementById('balance-total-amount');
+  const totalBalanceString = totalBalance.innerText;
+  const totalBalanceNumber = parseFloat(totalBalanceString);
+  const balanceTotal = totalWithdrawAmount - totalBalanceNumber;
+  totalBalance.innerText = Math.abs(balanceTotal);
+
+})
+
